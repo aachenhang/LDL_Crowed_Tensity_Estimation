27fb7ad17f0bfb5575a9c4555c262616505654f7
aachenhang <631150134@qq.com>
2018-04-07 00:11:29 +0800

    update init_lmdb_Expo.py

--- init_lmdb_Expo.py
+++ init_lmdb_Expo.py
@@ -10,7 +10,7 @@ import numpy as np
 from random import shuffle
 import scipy
 import os, re
-from os.path import join, splitext, split, abspath, isdirA
+from os.path import join, splitext, split, abspath, isdir
 import matplotlib.pyplot as plt
 import copy
 
@@ -35,8 +35,8 @@ def is_image(im):
 max_count = max([int(re.sub("[^0-9]", "", img.split('n')[-1])) for img in os.listdir(join(args.data, 'test'))+os.listdir(join(args.data, 'train_frame')) if is_image(img)])
 min_count = min([int(re.sub("[^0-9]", "", img.split('n')[-1])) for img in os.listdir(join(args.data, 'test'))+os.listdir(join(args.data, 'train_frame')) if is_image(img)])
 mean_count = np.mean(np.array([int(re.sub("[^0-9]", "", img.split('n')[-1])) for img in os.listdir(join(args.data, 'test'))+os.listdir(join(args.data, 'train_frame')) if is_image('.JPG')], dtype=np.float))
-#如果人数范围超过了50，则
-scale = float(1.0*max_count / 50.0)
+#如果人数范围超过了100，则
+scale = float(1.0*max_count / 100.0)
 
 if(scale > 1):
     max_count = int(max_count / scale)
@@ -49,7 +49,7 @@ gaussian = scipy.signal.gaussian(max_count - min_count + 1, args.std)
 
 # 制作标签
 def make_label(label_value):
-    label_value = int(1.0*label_value / scale)
+    label_value = int(round(1.0*label_value / scale))
     label_value = label_value - min_count
     label_distr = np.zeros([max_count - min_count +1])
     mid =  np.ceil((max_count - min_count +1)/2)
@@ -81,12 +81,12 @@ def make_lmdb(db_path, img_list, data_type = 'image', phase = 'train'):
     os.makedirs(db_path)
     db = lmdb.open(db_path, map_size=int(1e12))
     with db.begin(write=True) as in_txn:
-        for idx, im in enumerate(img_list):  # index , img
+        for idx, img in enumerate(img_list):  # index , img
             if data_type == 'image':
                 if(phase == 'train'):
-                    data = np.array(Image.open(os.path.join(args.data, 'train_frame', im)), dtype=np.float)
+                    data = np.array(Image.open(os.path.join(args.data, 'train_frame', img)), dtype=np.float)
                 else:
-                    data = np.array(Image.open(os.path.join(args.data, 'test', im)), dtype=np.float)
+                    data = np.array(Image.open(os.path.join(args.data, 'test', img)), dtype=np.float)
                 data = scipy.misc.imresize(data, (360,288))
 
                 data = data[:, :, ::-1]  # rgb to bgr
